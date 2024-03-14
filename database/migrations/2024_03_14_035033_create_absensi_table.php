6d1e5181f3e73c61567bcc1a5e5efa0114ee9af5
Aldi-H <aldihimawan88@gmail.com>
2024-03-14 11:11:58 +0700

    Initial Commit

---
+++ database/migrations/2024_03_14_035033_create_absensi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsensiTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('absensi', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('id_kelas');
+      $table->dateTime('waktu_masuk');
+      $table->dateTime('waktu_keluar');
+      $table->string('kode_absen');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('absensi');
+  }
+}
