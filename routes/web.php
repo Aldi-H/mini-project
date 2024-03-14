89adce15fb26324d17a3d5b125343ab10c8bfec2
Aldi-H <aldihimawan88@gmail.com>
2024-03-14 18:05:26 +0700

    Refactor login and logout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +21,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function () {
+  return view('index');
+});
